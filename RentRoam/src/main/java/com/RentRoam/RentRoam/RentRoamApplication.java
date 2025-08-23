68db1ea63032ebb3f9e2d3af7df46c7945f1118e
Dhyani <dhyanipatel1001052gmail.com>
2025-08-23 13:03:45 +0530

    init commit

---
+++ RentRoam/src/main/java/com/RentRoam/RentRoam/RentRoamApplication.java
@@ -0,0 +1,13 @@
+package com.RentRoam.RentRoam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentRoamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentRoamApplication.class, args);
+	}
+
+}
---
+++ RentRoam/src/test/java/com/RentRoam/RentRoam/RentRoamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RentRoam.RentRoam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentRoamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
